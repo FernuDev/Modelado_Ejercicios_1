3de1a867b4c84b67b1ccc50519cbc2985898f81e
FernuDev <fernu292@gmail.com>
2025-03-12 17:35:32 -0600

    add: Adding git ignore and Readme.md

--- main.cpp
+++ main.cpp
@@ -1,19 +1,7 @@
 #include <iostream>
 #include "./src/Solution.h"
 
-int main() {
-
-    const std::vector<std::vector<int>> matrix = { {1,2}, {2,4} };
-    const std::vector<int> respuesta = Solution::get_repeated_missing_number(matrix);
-    std::cout << "\nRepetido: " << respuesta[0] << ", Faltante: " << respuesta[1] << std::endl;
-
-    const std::string roman = "LVIII";
-    const int number = Solution::roman_to_integer(roman);
-    std::cout << number << std::endl;
-
-    constexpr int num = 456;
-    const std::string roman_solution = Solution::integer_to_roman(num);
-    std::cout << roman_solution << std::endl;
-
-    return 0;
+int main(int argc, char **argv) {
+    std::cout << "Ejercicios 1" << std::endl;
+    return EXIT_SUCCESS;
 }
--- src/Solution.h
+++ src/Solution.h
@@ -12,7 +12,7 @@ public:
     static std::vector<int> get_repeated_missing_number(const std::vector<std::vector<int> > &matrix);
     static std::string integer_to_roman(int number);
     static int roman_to_integer(const std::string &roman);
-    static std::string get_biggest_prefix(std::vector<std::string> words);
+    static std::string get_biggest_prefix(const std::vector<std::string> &words);
 };
 
 #endif //SOLUTION_H
